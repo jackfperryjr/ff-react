235cd7527348186afb9d467d0f3cd44cbfd87811
jackfperryjr <jack.franklin.perryjr@gmail.com>
2019-05-01 07:23:41 -0400

    Updates

---
+++ precache-manifest.991912a886bef13b3b662c61e4a3831c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c1075efee9e3b7e2e8befd176e7a2e",
+    "url": "/ff-react/index.html"
+  },
+  {
+    "revision": "c9a3bf705879d27c1a41",
+    "url": "/ff-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "56717a703d890728ecaf",
+    "url": "/ff-react/static/css/main.1fffb852.chunk.css"
+  },
+  {
+    "revision": "c9a3bf705879d27c1a41",
+    "url": "/ff-react/static/js/2.71beebb5.chunk.js"
+  },
+  {
+    "revision": "56717a703d890728ecaf",
+    "url": "/ff-react/static/js/main.d47221b8.chunk.js"
+  },
+  {
+    "revision": "442954e9f1bf16584e1a",
+    "url": "/ff-react/static/js/runtime~main.b679b669.js"
+  },
+  {
+    "revision": "b747abc0ac2ae4de0498beb680fcd77e",
+    "url": "/ff-react/static/media/moogle-logo.b747abc0.png"
+  }
+]);
